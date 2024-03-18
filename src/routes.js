75a410080f4a04b4385c48647c8485db2e83968d
mattiiee <matboonmalert8@gmail.com>
2024-03-18 22:42:33 +0700

    fix path

--- src/routes.js
+++ src/routes.js
@@ -1,36 +1,47 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router"
 
 const routes = [
-  { path: '/', component: () => import('./components/HomePage.vue') },
-  { path: '/ranking', component: () => import('./components/Ranking/RankingSpec.vue') },
-  { path: '/build', component: () => import('./components/BuildSpec.vue') },
+  { path: "/", component: () => import("./components/HomePage.vue") },
   {
-    path: '/tiptrick',
-    component: () => import('./components/Article/ArticleTipTrick.vue'),
+    path: "/ranking",
+    component: () => import("./components/Rank/RankingSpec.vue"),
   },
   {
-    path: '/promotion',
-    component: () => import('./components/Article/ArticlePromotion.vue'),
-  },
-  { path: '/news', component: () => import('./components/Article/ArticleNews.vue') },
-  { path: '/howto', component: () => import('./components/Article/ArticleHowTo.vue') },
-  {
-    path: '/new/meow',
-    component: () => import('./components/Article/ArticlesNewsMeow.vue'),
+    path: "/build",
+    component: () => import("./components/BuildSpec.vue"),
   },
   {
-    path: '/new/redmi',
-    component: () => import('./components/Article/ArticleNewsRedMi.vue'),
+    path: "/tiptrick",
+    component: () => import("./components/Article/ArticleTipTrick.vue"),
   },
   {
-    path: '/new/geforce',
-    component: () => import('./components/Article/ArticlesNewsGefore.vue'),
+    path: "/promotion",
+    component: () => import("./components/Article/ArticlePromotion.vue"),
   },
   {
-    path: '/new/macbook',
-    component: () => import('./components/Article/ArticlesMacbook.vue'),
+    path: "/news",
+    component: () => import("./components/Article/ArticleNews.vue"),
   },
-  
+  {
+    path: "/howto",
+    component: () => import("./components/Article/ArticleHowTo.vue"),
+  },
+  {
+    path: "/new/meow",
+    component: () => import("./components/Article/ArticlesNewsMeow.vue"),
+  },
+  {
+    path: "/new/redmi",
+    component: () => import("./components/Article/ArticleNewsRedMi.vue"),
+  },
+  {
+    path: "/new/geforce",
+    component: () => import("./components/Article/ArticlesNewsGefore.vue"),
+  },
+  {
+    path: "/new/macbook",
+    component: () => import("./components/Article/ArticlesMacbook.vue"),
+  },
 ]
 
 const router = createRouter({
