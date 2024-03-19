59149fa96cb55ed6926d1f6db9d79756644c8cba
Nisarat Aunrueanngam <noansrnn@gmail.com>
2024-03-19 15:07:31 +0700

    modify ram id

--- src/components/Rank/RankingSpec.vue
+++ src/components/Rank/RankingSpec.vue
@@ -4,7 +4,7 @@ import TopRank from "./TopRank.vue";
 </script>
 
 <template>
-  <div class="bg-gray-100 dark:bg-slate-800 p-4 md:p-12">
+  <div class=" dark:bg-slate-800 p-4 md:p-12">
     <h1 class="text-3xl md:text-3xl font-bold  text-gray-900 dark:text-white mb-8">Ranking Spec</h1>
     <TopRank />
   </div>
--- src/components/Rank/TopRank.vue
+++ src/components/Rank/TopRank.vue
@@ -1,29 +1,48 @@
 <template>
     <div class="top">
-        <div class="rank border-solid bg-lime-300">
-            1
+
+        <div class="card w-96 glass">
+            <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="car!" />
+            </figure>
+            <div class="card-body">
+                <h2 class="card-title">Life hack</h2>
+                <p>How to park your car at your garage?</p>
+                <div class="card-actions justify-end">
+                    <button class="btn btn-primary">Learn now!</button>
+                </div>
+            </div>
         </div>
-        <div class="rank border-solid bg-lime-300">
-            2
+        <div class="card w-96 glass">
+            <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="car!" />
+            </figure>
+            <div class="card-body">
+                <h2 class="card-title">Life hack</h2>
+                <p>How to park your car at your garage?</p>
+                <div class="card-actions justify-end">
+                    <button class="btn btn-primary">Learn now!</button>
+                </div>
+            </div>
         </div>
-        <div class="rank border-solid bg-lime-300">
-            3
+        <div class="card w-96 glass">
+            <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="car!" />
+            </figure>
+            <div class="card-body">
+                <h2 class="card-title">Life hack</h2>
+                <p>How to park your car at your garage?</p>
+                <div class="card-actions justify-end">
+                    <button class="btn btn-primary">Learn now!</button>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <style scoped>
 .top {
-    display: flex;
-    justify-content: space-around;
-    margin-top: 20px;
+  display: flex;
+  justify-content: space-around;
+  margin-top: 20px;
 }
 
-.rank {
-    width: 100px;
-    height: 100px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-}
+
 </style>
