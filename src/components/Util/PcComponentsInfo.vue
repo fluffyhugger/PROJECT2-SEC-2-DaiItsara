69348f93bf506290219902877fa4a469ac5d84bb
Sirapob <blacking987@gmail.com>
2024-03-20 19:41:41 +0700

    Update PcComponentsInfo.vue

--- src/components/Util/PcComponentsInfo.vue
+++ src/components/Util/PcComponentsInfo.vue
@@ -1,18 +1,37 @@
 <script setup>
-import { ref, onMounted } from "vue"
-import { useRoute } from "vue-router"
+import { ref, onMounted, onBeforeMount } from 'vue'
+import { useRoute } from 'vue-router'
 
 const calledComponent = ref({})
 const isLoading = ref(true)
 const route = useRoute()
 
-onMounted(async () => {
-  const result = await fetch(`http://localhost:5000/cpu/${route.params.ID}`)
-  const response = await result.json()
-  // console.log(response)
-  calledComponent.value = response
-  console.log(calledComponent.value)
-  isLoading.value = false
+const CpuComponent = ref(null)
+
+// Define an async function to use await inside the setup
+const fetchData = async () => {
+  try {
+    const result = await fetch(
+      `http://localhost:5000/cpu/?ID=${route.params.ID}`
+    )
+
+    if (!result.ok) {
+      throw new Error('Network response was not ok')
+    }
+
+    const response = await result.json()
+    calledComponent.value = response
+    CpuComponent.value = JSON.parse(JSON.stringify(calledComponent.value[0]))
+    console.log(CpuComponent.value.ID)
+    isLoading.value = false
+  } catch (error) {
+    console.error('Error fetching CPU data:', error)
+    // Optionally, you can set an error message or handle the error in your UI
+  }
+}
+
+onBeforeMount(async () => {
+  await fetchData()
 })
 </script>
 
@@ -28,8 +47,8 @@ onMounted(async () => {
       <div class="flex justify-center xl:w-1/2">
         <img
           class="h-80 w-80 sm:w-[28rem] sm:h-[28rem] flex-shrink-0 object-cover rounded-md"
-          :src="calledComponent.Picture"
-          :alt="calledComponent.ID"
+          :src="CpuComponent.Picture"
+          :alt="CpuComponent.ID"
         />
       </div>
 
@@ -39,8 +58,8 @@ onMounted(async () => {
         <h2
           class="text-3xl font-bold tracking-tight text-gray-800 xl:text-4xl dark:text-white"
         >
-          {{ calledComponent.Brand }} {{ calledComponent.Series }}
-          {{ calledComponent.Model }}
+          {{ CpuComponent.Brand }} {{ CpuComponent.Series }}
+          {{ CpuComponent.Model }}
         </h2>
       </div>
     </div>
