5cfba30fa1406e31ac803ca3c68fbb3e7b6a049e
Nisarat Aunrueanngam <noansrnn@gmail.com>
2024-03-21 05:29:29 +0700

    edit data

--- src/components/Rank/RankingSpec.vue
+++ src/components/Rank/RankingSpec.vue
@@ -6,7 +6,7 @@ import TopRank from "./TopRank.vue";
 <template>
   <div class="relative">
     <h1
-      class="text-3xl font-bold text-gray-900 tracking-wide mt-8 mb-6 bg-gradient-to-r from-green-400 via-green-500 to- bg-opacity-50 rounded-lg p-4 shadow-md hover:shadow-lg transition duration-300">
+      class="text-3xl font-bold text-gray-900 tracking-wide mt-8 mb-6 0 ">
       PC RANKING
     </h1>
     <TopRank />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      nct: "#39FF14",
+    },
   },
-  plugins: [require('daisyui')],
-}
+  plugins: [require("daisyui"), require("@tailwindcss/aspect-ratio")],
+};
