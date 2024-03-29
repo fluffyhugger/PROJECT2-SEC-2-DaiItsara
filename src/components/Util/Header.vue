2037cd9931adaf96d5c6e8794a05654285bbc387
Nisarat Aunrueanngam <101193445+Nisarat-A@users.noreply.github.com>
2024-03-29 13:13:22 +0700

    Update Header.vue

--- src/components/Util/Header.vue
+++ src/components/Util/Header.vue
@@ -1,34 +1,12 @@
-<template>
-  <header>
-    <div class="flex">
-      <RouterLink to="/">
-        <h1 class="text-3xl font-bold :hover cursor-pointer">PC BUILDER</h1>
-      </RouterLink>
-      <details class="dropdown ml-10 pt-2">
-        <summary class="m-1 font-bold">Custom Spec</summary>
-        <ul
-          class="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52"
-        >
-          <li><RouterLink to="/build">Modified Your PC</RouterLink></li>
-          <li><RouterLink to="/ranking">Top PC Ranking </RouterLink></li>
-        </ul>
-      </details>
-      <details class="dropdown ml-10 pt-2">
-        <summary class="m-1 font-bold">Articles</summary>
-        <ul
-          class="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52"
-        >
-          <li v-for="articleType in articleTypes" :key="articleType">
-            <RouterLink :to="`/${articleType}`">{{
-              articleType.charAt(0).toLocaleUpperCase() + articleType.slice(1)
-            }}</RouterLink>
-          </li>
-        </ul>
-      </details>
-      <div class="ml-10 pt-3">
-        <RouterLink to="/contact" class="m-1 font-bold :hover cursor-pointer"
-          >Contacts Us</RouterLink
-        >
+<!-- <template>
+  <header class="bg-gradient-to-b from-black to-neon-blue text-white py-4 shadow-header">
+    <div class="container mx-auto flex items-center justify-between">
+      <div class="flex items-center">
+        <router-link to="/" class="hover:text-neon-blue">
+          <h1 class="text-3xl font-bold ml-2 hover:text-neon-blue transition-colors">
+            DAIITSARA PC
+          </h1>
+        </router-link>
       </div>
       <nav class="space-x-6">
         <router-link to="/build" class="hover:text-neon-blue">PC Build</router-link>
