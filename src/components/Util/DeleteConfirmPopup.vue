48dd25359562b540633c2a0f189087f5195acc5d
Chocolaz <sirakran.kran@mail.kmutt.ac.th>
2024-04-03 19:33:05 +0700

    Filtered CPU Socket Fixed

--- src/components/Util/DeleteConfirmPopup.vue
+++ src/components/Util/DeleteConfirmPopup.vue
@@ -30,7 +30,7 @@ export default {
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
+  background-color: rgba(0, 0, 0, 0.178);
   display: flex;
   justify-content: center;
   align-items: center;
@@ -40,7 +40,7 @@ export default {
   background-color: white;
   padding: 20px;
   border-radius: 8px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.096);
   max-width: 400px;
   text-align: center;
 }
